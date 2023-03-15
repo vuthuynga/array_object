6da81f7ebe67899f958344b8a74d4f6b1297b7db
tran-tuong <tranvantuong2k3@gmail.com>
2023-03-15 22:38:26 +0700

    check

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 // ex1
-let myInfo = {
-    name: 'Vu Thi Thuy Nga',
-    year_of_birth: 2004,
-    address: "Ha Noi",
-    getAge: function(name, year_of_birth) {
-        var d = new Date();
-        var current_year = d.getFullYear();
+// let myInfo = {
+//     name: 'Vu Thi Thuy Nga',
+//     year_of_birth: 2004,
+//     address: "Ha Noi",
+//     getAge: function(name, year_of_birth) {
+//         var d = new Date();
+//         var current_year = d.getFullYear();
 
-        var age = current_year - year_of_birth;
-        console.log(name + " is " + age + " years old");
-}
-    }
-console.log(myInfo)
-console.log(myInfo.getAge())
+//         var age = current_year - year_of_birth;
+//         console.log(name + " is " + age + " years old");
+// }
+//     }
+// console.log(myInfo)
+// console.log(myInfo.getAge())
+
+// let myInfo = {
+//     name: 'Vu Thi Thuy Nga',
+//     year_of_birth: 2004,
+//     address: "Ha Noi",
+//     getAge: function(name, year_of_birth) {
+//         let d = new Date();
+//         let current_year = d.getFullYear();
+
+//         let age = current_year - this.year_of_birth;
+//         return this.name + " is " + age + " years old"
+// }
+//     }
+// console.log(myInfo)
 
 //True code with true demand
 // function run(name, year_of_birth) {
@@ -41,54 +55,6 @@ console.log(myInfo.getAge())
 // ex3
 // cau nay em can tim hieu them 
 
-// ex4
-let array1 = [1,0,2,3,4];
-let array2 = [3,5,6,7,8,13];
-let sum = []
-for(var i = 0; i < array1.length; i++){
-    sum.push(array1[i] + array2[i]);
- }
-console.log(sum)
-
-
-// ex5
-let library = [ 
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        readingStatus: true
-    },
-    {
-        author: 'Steve Jobs',
-        title: 'Walter Isaacson',
-        readingStatus: true
-    },
-    {
-        author: 'Suzanne Collins',
-        title:  'Mockingjay: The Final Book of The Hunger Games', 
-        readingStatus: false
-    }];
-let need = function() {
-    if(readingStatus == true) {
-        console.log('Already read ',title, 'by', author )
-    }
-    else {
-        console.log('You still need to read', title, 'by', author)
-    }
-}
-console.log(need)
-
-
-// ex6
-var student = {
-    name : "David Rayy",
-    sclass : "VI",
-    rollno : 12 
-}
-
-console.log(move([10, 20, 30, 40, 50], 0, 2));
-
-console.log(move([10, 20, 30, 40, 50], -1, -2));
 // // ex4
 //Bai nay cung bo vao function nhe, nhung em nen bo sung them dieu kien neu hai mang khong co do dai bang nhau thi tinh nhu nao
 // let array1 = [1,0,2,3,4];
@@ -127,19 +93,8 @@ console.log(move([10, 20, 30, 40, 50], -1, -2));
 //     }
 // }
 // console.log(need)
-//True code
-// let renderFunction = (library) => {
-//   let map = "";
-//   for (let index = 0; index < library.length; index++) {
-//     if (library[index].readingStatus) {
-//       map += `Already read ${library[index].title} by ${library[index].author} \n`;
-//     } else {
-//       map += `You still need to read ${library[index].title} by ${library[index].author} \n`;
-//     }
-//   }
-//   console.log(map);
-// };
-// renderFunction(library);
+
+
 // // ex6
 //Bài nay dung rui
 // var student = {
