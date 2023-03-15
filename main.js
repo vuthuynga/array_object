5f91550dd2e4016906d59eb8e40911b977ec03ee
tran-tuong <tranvantuong2k3@gmail.com>
2023-03-15 22:40:03 +0700

    Check-js

--- main.js
+++ main.js
@@ -54,7 +54,25 @@
 
 // ex3
 // cau nay em can tim hieu them 
+function move(arr, old_index, new_index) {
+    while (old_index < 0) {
+        old_index += arr.length;
+    }
+    while (new_index < 0) {
+        new_index += arr.length;
+    }
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length;
+        while ((k--) + 1) {
+            arr.push(undefined);
+        }
+    }
+     arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);  
+   return arr;
+}
 
+console.log(move([10, 20, 30, 40, 50], 0, 2));
+console.log(move([10, 20, 30, 40, 50], -1, -2));
 // // ex4
 //Bai nay cung bo vao function nhe, nhung em nen bo sung them dieu kien neu hai mang khong co do dai bang nhau thi tinh nhu nao
 // let array1 = [1,0,2,3,4];
